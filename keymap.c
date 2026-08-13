69a10f2419bf0b8b1cb0cd6208e8694b38bbb4fe
fort <jm8359553@gmail.com>
2026-08-13 19:56:17 +0530

    Add initial keymap configuration in keymap.c

---
+++ keymap.c
@@ -0,0 +1,9 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_A,    // Key 1
+        KC_B,    // Key 2
+        KC_C     // Key 3
+    )
+};
